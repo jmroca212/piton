bc6a4d5274b39612049880d3942d56bbf13b0412
jose roca <jmroca@grupopayne.com.ar>
2019-01-17 13:52:11 -0300

    un nuevo menu nada
    probando kraken

--- index.py
+++ index.py
@@ -20,6 +20,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/nada', strict_slashes=False)
+def about():
+    return render_template("nada.html")
+
 # Make sure this we are executing this file
 if __name__ == '__main__':
     app.run(debug=True)
