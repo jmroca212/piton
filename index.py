29a6ef861f32f4cde41a6cde8a4994c4e6e68b76
jose roca <jmroca@grupopayne.com.ar>
2019-01-18 12:53:41 -0300

    sumit de gitkraken

--- index.py
+++ index.py
@@ -2,14 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Creating simple Routes 
-@app.route('/test')
-def test():
-    return "Home Page"
 
-@app.route('/test/about/')
-def about_test():
-    return "About Page"
 
 # Routes to Render Something
 @app.route('/')
